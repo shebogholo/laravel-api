dcc83936d3aa3099b207d3a89c2368c2fb108581
Shebogholo <shebogholo@gmail.com>
2021-04-29 20:08:34 +0300

    added business model

--- app/Http/Controllers/Api/BusinessController.php
+++ app/Http/Controllers/Api/BusinessController.php
@@ -5,18 +5,37 @@ namespace App\Http\Controllers\Api;
 use App\Models\Business;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Http\Resources\BusinessResource;
+use Illuminate\Support\Facades\Validator;
 
 class BusinessController extends Controller
 {
     public function index()
     {
         $businesses = Business::all();
+        return response(['businesses' =>  BusinessResource::collection($businesses)], 200);
     }
 
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required|max:255',
+            'location' => 'required|max:255',
+            'region' => 'required|max:255',
+            'number' => 'required',
+            'website' => '',
+            'logo' => ''
+        ]);
+
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors(), 'Validation Error']);
+        }
+        // return $data;
+
+        $business = Business::create($data);
+        return response(['business' => new BusinessResource($business), 'message' => 'Created successfully'], 200);
     }
 
     public function show(Business $business)
@@ -29,7 +48,7 @@ class BusinessController extends Controller
         //
     }
 
-    
+
     public function destroy(Business $business)
     {
         //
--- app/Models/Business.php
+++ app/Models/Business.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Business extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name', 'location', 'region', 'number', 'website', 'logo'
+    ];
 }
