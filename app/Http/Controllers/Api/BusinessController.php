ccc66fc32148428a5670b83a488814839116a1bd
Shebogholo <shebogholo@gmail.com>
2021-04-30 08:00:40 +0300

    added data.json, db operations

--- app/Http/Controllers/Api/BusinessController.php
+++ app/Http/Controllers/Api/BusinessController.php
@@ -18,6 +18,35 @@ class BusinessController extends Controller
 
     public function store(Request $request)
     {
+        // Read data from storage folder and insert to database
+        $json = file_get_contents(storage_path('data.json'));
+        $objects = json_decode($json, true);
+        foreach($objects as $object){
+            $data = [
+                'name' => $object['business_title'],
+                'location' => $object['business_location'],
+                'region' => $object['business_region'],
+                'number' => $object['business_number'],
+                'website' => $object['business_website'],
+                'logo' => $object['business_logo']
+            ];
+            $validator = Validator::make($data, [
+                'name' => 'required|max:255|unique:businesses',
+                'location' => 'required|max:255',
+                'region' => 'required|max:255',
+                'number' => 'required',
+                'website' => '',
+                'logo' => ''
+            ]);
+    
+            if ($validator->fails()) {
+                return response(['error' => $validator->errors()->first()]);
+            }
+            $business = Business::create($data);
+        }
+        return response(['message' => 'All businesses already inserted into database.'], 200);
+        
+        // Get data from request
         $data = $request->all();
 
         $validator = Validator::make($data, [
@@ -40,7 +69,15 @@ class BusinessController extends Controller
 
     public function show(Business $business)
     {
-        //
+        
+        // Todo: I dont like the error message if item is not found, custom message?
+
+        // unset multiple fields
+        $remove = array('id', 'created_at', 'updated_at');
+        foreach($remove as $key){
+            unset($business[$key]);
+        }
+        return response(['business' => new BusinessResource($business)]);
     }
 
     public function update(Request $request, Business $business)
@@ -51,6 +88,7 @@ class BusinessController extends Controller
 
     public function destroy(Business $business)
     {
-        //
+        $business->delete();
+        return response(['message' => 'Deleted']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,8 @@ Route::post('/register', 'App\Http\Controllers\Api\AuthController@register');
 Route::post('/login', 'App\Http\Controllers\Api\AuthController@login');
 
 Route::apiResource('/businesses', 'App\Http\Controllers\Api\BusinessController')->middleware('auth:api');
+
+// Incase anything goes wrong
+Route::fallback(function(){
+    return response(['error' => 'Resource not found!'], 404);
+})->name('fallback');
